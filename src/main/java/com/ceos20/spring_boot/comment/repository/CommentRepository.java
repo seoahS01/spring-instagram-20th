550f468b00af922200f2de0e4e675de9381558a4
Seoah <0325sandy@ewhain.net>
2024-09-29 13:55:58 +0900

    update 단위 테스트

--- src/main/java/com/ceos20/spring_boot/comment/repository/CommentRepository.java
+++ src/main/java/com/ceos20/spring_boot/comment/repository/CommentRepository.java
@@ -2,12 +2,18 @@ package com.ceos20.spring_boot.comment.repository;
 
 import com.ceos20.spring_boot.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     // 특정 게시글에 달린 댓글 수 조회
     Long countByPostId(Long postId);
+
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.postId = :postId")
+    List<Comment> findAllByPostIdWithUser(@Param("postId") Long postId);
 
 }
